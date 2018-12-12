617656ac0240b8a436263ff5be8b9d2b23e75ab7
Simone Taeggi <startae14@gmail.com>
2018-12-12 16:25:06 +0100

    Add content and behavior into the Faq Component

--- src/components/Faq/Faq.js
+++ src/components/Faq/Faq.js
@@ -1,10 +1,55 @@
 import React, { Component } from 'react';
+import './Faq.css';
+import axios from 'axios';
 
 class Faq extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            faqs: []
+        };
+    }
+    componentDidMount() {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                const faqs = response.data.slice(0, 10);
+                this.setState({ faqs });
+            })
+            .catch(e => {
+                this.errors.push(e);
+            });
+    }
     render() {
         return (
             <div>
-                FAQ
+                <div className="container">
+                    <section className="section">
+                        <h1 className="title">
+                            FAQ
+                        </h1>
+                        <h2 className="subtitle is-4">
+                            Lorem ipsum and all of that jazz.
+                        </h2>
+                        <div className="columns">
+                            {
+                                this.state.faqs.map(faq => (
+                                    <div className="column is-one-third">
+                                        <div className="card">
+                                            <div className="card-content">
+                                                <p className="title">
+                                                    { faq.title }
+                                                </p>
+                                                <p className="answer">
+                                                    { faq.body }
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </section>
+                </div>
             </div>
         );
     }
